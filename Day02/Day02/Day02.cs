d43d3d680555e47066d7f25de4b16fd25f5bc843
Garrett Girod <girodg@hotmail.com>
2022-06-01 10:01:28 -0400

    ListChallenge

--- Day02/Day02/Day02.cs
+++ Day02/Day02/Day02.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day02
 {
@@ -15,10 +12,44 @@ namespace Day02
             Console.WriteLine("----------------");
             for (int i = 0; i < best.Length; i++)
             {
-                Console.WriteLine(best[i]);
+                Console.WriteLine(best[i]);//indexer: O(1). constant.
             }
             best[1] = "Aquaman";
             ArrayChallenge();
+
+            // = new  that is creating an instance
+            List<string> bestList = new List<string>() { "Batman" };
+            bestList.Add("Flash");
+            bestList.Add("Wonder Woman");
+
+            ListChallenge();
+        }
+
+        static void ListChallenge()
+        {
+            List<double> grades = new List<double>();
+            Random randy = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                //0.0 - 1.0 like a percentage
+                grades.Add(randy.NextDouble() * 101);
+            }
+        }
+
+        static int Find(string[] names, string itemToFind)
+        {
+            //Performance: O(N) linear.
+            int index = -1;
+            int N = names.Length;
+            for (int i = 0; i < N; i++)
+            {
+                if (names[i] == itemToFind)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
         }
 
         static void ArrayChallenge()
