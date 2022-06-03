ae26399e91e8c91cf27d5851893294f259a26490
Garrett Girod <girodg@hotmail.com>
2022-06-03 09:31:08 -0400

    Day03

---
+++ Day03/Day03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //3 ways to add data:
+            //1) initializer -- will throw an exception if the key is already in the dictionary
+            //2) .Add -- will throw an exception if the key is already in the dictionary
+            //3) [] -- will NOT throw an exception. Will overwrite the value.
+            Dictionary<string, float> menu = new Dictionary<string, float>()
+            {
+                //{ key, value } KeyValuePair
+                {"Spaghetti", 5.99F },
+                {"Lasagna", 8.99F }
+            };
+            menu.Add("Ravioli", 9.99F);
+            menu.Add("Cheese Pizza", 12.99F);
+
+            menu["Garlic Bread"] = 5.99F;
+
+        }
+    }
+}
