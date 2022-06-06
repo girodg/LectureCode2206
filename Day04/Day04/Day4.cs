a1887dbfe612073a59c7722b23d12249c9a14741
Garrett Girod <girodg@hotmail.com>
2022-06-06 10:06:54 -0400

    ReadData

--- Day04/Day04/Day4.cs
+++ Day04/Day04/Day4.cs
@@ -28,6 +28,35 @@ namespace Day04
                 sw.Write(delimiter);
                 sw.Write(12);
             }//3. Close the file!
+
+            ReadData(filePath);
+        }
+
+        private static void ReadData(string filePath)
+        {
+            char delimiter = '^';
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+
+                string[] data = line.Split(delimiter);
+                foreach (var item in data)
+                {
+                    Console.WriteLine(item);
+                }
+                //Console.WriteLine(data[0]);
+                //int score = int.Parse(data[1]);
+            }
+            // ALTERNATE WAY of reading
+            //...use File.ReadAllText
+            //
+            string lineData = File.ReadAllText(filePath); //does all 3 steps: open, read, close
+            string[] data2 = lineData.Split(delimiter);
+            foreach (var item in data2)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
