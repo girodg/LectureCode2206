7ece2ab06e322b730e9a43e2cc56f0e9828cb04b
Garrett Girod <girodg@hotmail.com>
2022-06-06 09:50:49 -0400

    Write CSV

---
+++ Day04/Day04/Day4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Day04
+{
+    internal static class Day4
+    {
+        public static void Run()
+        {
+            string filePath = @"C:\temp\2206\HighScores.txt";
+            char delimiter = '^';
+            //1. open the file
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                //2. Write to the file
+                sw.Write("Batman");
+                sw.Write(delimiter);
+                sw.Write(10000000);
+                sw.Write(delimiter);
+                sw.Write("Aquaman");
+                sw.Write(delimiter);
+                sw.Write(-50);
+                sw.Write(delimiter);
+                sw.Write(true);
+                sw.Write(delimiter);
+                sw.Write(57.3);
+                sw.Write(delimiter);
+                sw.Write(12);
+            }//3. Close the file!
+        }
+    }
+}
---
+++ Day04/Day04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day04.Day4.Run();
+        }
+    }
+}
