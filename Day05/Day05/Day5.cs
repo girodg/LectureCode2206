c0c3203b3d0c9f5711bc2584c5e7a84d2a8fea1a
Garrett Girod <girodg@hotmail.com>
2022-06-06 12:13:31 -0400

    Recursion

---
+++ Day05/Day05/Day5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05
+{
+    internal class Day5
+    {
+
+        static Random rando = new Random();
+        public static void Run()
+        {
+            while (true)
+            {
+                if (rando.Next(100) == 50) break;
+            }
+            RandoRecursive();
+
+            Console.WriteLine("---------iterative loop---------");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("---------recursive loop---------");
+            int N = 1;
+            Counter(N);
+            Console.ResetColor();
+        }
+
+        private static void Counter(int N)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(N);
+            if (N < 100)
+                Counter(N + 1);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(N);
+        }
+
+        static void RandoRecursive()
+        {
+            if (rando.Next(100) == 50)
+                return;//break
+            RandoRecursive();
+        }
+    }
+}
---
+++ Day05/Day05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day5.Run();
+        }
+    }
+}
