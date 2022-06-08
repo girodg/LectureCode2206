74322dec22287ad577c771cf20c9ab78ffc82a09
Garrett Girod <girodg@hotmail.com>
2022-06-08 11:05:28 -0400

    Swap and CompareTo

--- Day06/Day06/Day6.cs
+++ Day06/Day06/Day6.cs
@@ -10,7 +10,30 @@ namespace Day06
     {
         public static void Run()
         {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6 };
+            Swap(nums, 2, 3);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
 
+            Console.WriteLine();
+            string s1 = "Batman", s2 = "Batmen";
+            // -1: less than
+            //  0: equal to
+            //  1: greater than
+            int compResult = s1.CompareTo(s2);
+            if (compResult == 0) Console.WriteLine($"{s1} EQUALS {s2}");
+            else if(compResult == -1) Console.WriteLine($"{s1} LESS THAN {s2}");
+            else if (compResult == 1) Console.WriteLine($"{s1} GREATER THAN {s2}");
+        }
+
+        private static void Swap(int[] nums, int ndx1, int ndx2)
+        {
+            //int temp = nums[ndx1];
+            //nums[ndx1] = nums[ndx2];
+            //nums[ndx2] = temp;
+            (nums[ndx2], nums[ndx1]) = (nums[ndx1], nums[ndx2]);
         }
     }
 }
